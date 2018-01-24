126594a6a021c69d8d19fcb335237d489da9f9f9
fluffmetal <35767269+fluffmetal@users.noreply.github.com>
2018-01-24 20:54:36 +0700

    Workshop2 20012561
    NodeMCUBylnk1.ino
    ledPin123_D0D4D5.ino
    at Maker Faire Bangkok 2018 20-21 January 2018 at The Street Ratchada

---
+++ ledPin123_D0D4D5.ino
@@ -0,0 +1,30 @@
+#define ledPin1 D0
+#define ledPin2 D4
+#define ledPin3 D5
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode (ledPin1,OUTPUT);
+  pinMode (ledPin2,OUTPUT);
+  pinMode (ledPin3,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(ledPin1, 0);
+  digitalWrite(ledPin2, 0);
+  digitalWrite(ledPin3, 1);
+  delay(1000);
+  digitalWrite(ledPin1, 0);
+  digitalWrite(ledPin2, 1);
+  digitalWrite(ledPin3, 1);
+  delay(1000);
+  digitalWrite(ledPin1, 1);
+  digitalWrite(ledPin2, 1);
+  digitalWrite(ledPin3, 1);
+  delay(1000);
+  digitalWrite(ledPin1, 0);
+  digitalWrite(ledPin2, 0);
+  digitalWrite(ledPin3, 0);
+  delay(1000);
+}
