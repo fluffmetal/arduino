b342a8d4dcfad3c126cdf971d82df0ffcb8091d4
fluffmetal <35767269+fluffmetal@users.noreply.github.com>
2018-01-30 23:27:15 +0700

    go

--- PWM-1/PWM-1.ino
+++ PWM-1/PWM-1.ino
@@ -1,7 +1,7 @@
-#define R_pin D5  // Red
+#define R_pin D0  // Red
 #define Y_pin D4  // Yellow
 #define G_pin D5  // Green
-int st_R = 1023;
+int st_R = 0;
 int st_Y = 0;
 int st_G = 0;
 
@@ -16,44 +16,42 @@ void loop()
 {
   for (int i=0; 1<1024; i++)
   {
-    st_R--;
-    st_Y++;
+    st_G = 0;
+    st_R++;
     displayPWM(st_R,st_Y,st_G);
-    delay(30);
+    delay(10);
+  }
+  for (int i=0; i<1024; i++)
+  {
+    st_R--;
+    displayPWM(st_R,st_Y,st_G);
+    delay(10);
   }
   for (int i=0; i<1024; i++)
   {
     st_R = 0;
+    st_Y++;
+    displayPWM(st_R,st_Y,st_G);
+    delay(10);
+  }
+  for (int i=0; i<1024; i++)
+  {
     st_Y--;
     displayPWM(st_R,st_Y,st_G);
-    delay(30);
+    delay(10);
   }
   for (int i=0; i<1024; i++)
   {
     st_Y = 0;
     st_G++;
     displayPWM(st_R,st_Y,st_G);
-    delay(30);
+    delay(10);
   }
   for (int i=0; i<1024; i++)
   {
-    st_Y = 0;
     st_G--;
     displayPWM(st_R,st_Y,st_G);
-    delay(30);
-  }
-  for (int i=0; i<1024; i++)
-  {
-    st_R++;
-    st_G = 0;
-    displayPWM(st_R,st_Y,st_G);
-    delay(30);
-  }
-  for (int i=0; i<1024; i++)
-  {
-    st_R--;
-    displayPWM(st_R,st_Y,st_G);
-    delay(30);
+    delay(10);
   }
 }
 
