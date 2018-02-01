ce97421fd456920b8632d41ef904482f2bc26544
fluffmetal <35767269+fluffmetal@users.noreply.github.com>
2018-02-01 21:19:46 +0700

    LED

--- PinLEDD0/PinLEDD0.ino
+++ PinLEDD0/PinLEDD0.ino
@@ -1,12 +1,15 @@
 const int PIN_LED = D0;
   // put your setup code here, to run once:
+void setup()
+{
   pinMode(PIN_LED,OUTPUT);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
   digitalWrite(PIN_LED,HIGH);
-  delay(1000);
+  delay(500);
   digitalWrite(PIN_LED,LOW);
-  delay(1000);
+  delay(500);
 }
