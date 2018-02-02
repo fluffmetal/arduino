fe66b2bb2eb3b722cb2d5a09000a5cbb7a2a2291
fluffmetal <35767269+fluffmetal@users.noreply.github.com>
2018-02-02 23:43:30 +0700

    02022561

--- AWIFI-Switch3/AWIFI-Switch3.ino
+++ AWIFI-Switch3/AWIFI-Switch3.ino
@@ -9,10 +9,11 @@ Adafruit_NeoPixel SLEDs = Adafruit_NeoPixel (NUMSLEDs,PINSLEDs,NEO_RGB + NEO_KHZ
 #define ledPin2 D7
 int st_sw1 = 0;
 int st_sw2 = 0;
+// calm switch is 1 (signal) & press switch is 0 (signal)
 int st_lp1 = 0;
 int st_lp2 = 0;
-int last_st_sw1 = 1;
-int last_st_sw2 = 1;
+int last_st_sw1 = 0; // set 0 for When press to turn led on immediately
+int last_st_sw2 = 1; // set 1 for When press and after calm to turn led on immediately
 
 void setup()
 {
@@ -30,7 +31,7 @@ void loop()
   st_sw1 = digitalRead(sw1);
   if ((st_sw1 == 0) && (last_st_sw1 == 1))
   {
-    st_sw1 = ~st_sw1;
+    st_lp1 = ~st_lp1;
     digitalWrite(ledPin1,st_lp1);
     delay(250);
   }
@@ -39,7 +40,7 @@ void loop()
   st_sw2 = digitalRead(sw2);
   if ((st_sw2 == 1) && (last_st_sw2 == 0))
   {
-    st_sw2 = ~st_sw2;
+    st_lp2 = ~st_lp2;
     digitalWrite(ledPin2,st_lp2);
     delay(250);
   }
