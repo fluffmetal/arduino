705d8e0ef651c4933ffeb2f888d21506c3506dda
fluffmetal <35767269+fluffmetal@users.noreply.github.com>
2018-02-03 02:51:15 +0700

    BuzzerTone03022561

--- BuzzerTone/BuzzerTone.ino
+++ BuzzerTone/BuzzerTone.ino
@@ -8,9 +8,19 @@ void setup()
 
 void loop()
 {
-  analogWriteFreq(440); // 440Hz
-  delay(500);
-  analogWriteFreq(587); // 587Hz
-  delay(500);
+  for (int i = 0; i < 1; i++)
+  {
+    analogWriteFreq(440); // 440Hz
+    delay(500);
+    analogWriteFreq(587); // 587Hz
+    delay(500);
+  }
+  for (int i = 0; i <100; i++)
+  {
+    analogWriteFreq(67);
+    delay(250);
+    analogWriteFreq(50);
+    delay(300);
+  }
 }
 
