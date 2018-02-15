a1bbe7ee6f037cee41eb858a0e88658d01226272
fluffmetal <35767269+fluffmetal@users.noreply.github.com>
2018-02-16 02:24:46 +0700

    OLED

---
+++ AWIFI-TestOLED/AWIFI-TestOLED.ino
@@ -0,0 +1,26 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define OLED_RESET -1
+Adafruit_SSD1306 OLED(OLED_RESET);
+
+void setup()
+{
+  Serial.begin(9600);
+  OLED.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  // initialize with the I2C addr 0x3C (for the 128x64)
+}
+
+void loop()
+{
+  OLED.clearDisplay();
+  OLED.setTextColor(WHITE);
+  OLED.setCursor(0,0);
+  OLED.setTextSize(1);
+  OLED.printIn("Size1");
+  OLED.serCursor(35,20);
+  OLED.setTextSize(2);
+  OLED.printIn("Size2");
+  OLED_display();
+}
